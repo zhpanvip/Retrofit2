cbe2b2424ffa5d997e4f4290a2f0c0fa5f2d93c7
zhpan <zhangp@oitsme.com>
2017-11-02 10:36:36 +0800

    优化token处理

--- app/src/main/java/com/cypoem/retrofit/net/IdeaApiService.java
+++ app/src/main/java/com/cypoem/retrofit/net/IdeaApiService.java
@@ -4,11 +4,17 @@ import com.cypoem.retrofit.module.BasicResponse;
 import com.cypoem.retrofit.module.bean.MeiZi;
 
 import java.util.List;
+import java.util.Map;
 
 import io.reactivex.Observable;
+import okhttp3.RequestBody;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 import retrofit2.http.Query;
+import retrofit2.http.Url;
 
 /**
  * Created by dell on 2017/4/1.
@@ -35,5 +41,14 @@ public interface IdeaApiService {
     @Headers("Cache-Control: public, max-age=100")//设置缓存 缓存时间为100s
     @GET("everySay/selectAll.do")
     Observable<BasicResponse<List<MeiZi>>> lookBack(@Query("page") int page, @Query("rows") int number);
+
+
+    @POST("upload/uploadFile.do")
+    Observable<BasicResponse> uploadFiles(@Part("filename") String description,
+                                          @Part("pic\"; filename=\"image1.png") RequestBody imgs1,
+                                          @Part("pic\"; filename=\"image2.png") RequestBody imgs2);
+
+    @POST("upload/uploadFile.do")
+    Observable<BasicResponse> uploadFiles(@Part("filename") String description, @PartMap() Map<String, RequestBody> maps);
 
 }
