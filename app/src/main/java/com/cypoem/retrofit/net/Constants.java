888cd3730370987635c40482515a0623954e447d
zhpan <zhangp@oitsme.com>
2018-03-12 15:16:01 +0800

    添加HostnameVerifier验证hostname

--- app/src/main/java/com/cypoem/retrofit/net/Constants.java
+++ app/src/main/java/com/cypoem/retrofit/net/Constants.java
@@ -9,11 +9,9 @@ public interface Constants {
      * 网络请求超时时间毫秒
      */
     int DEFAULT_TIMEOUT = 20000;
-    String IP="202.108.22.5 ";
+    String IP="202.108.22.59";
     String HOST = "http://gank.io/";
     String API_SERVER_URL = HOST + "api/data/";
-    //  String HOST = "https://180.101.147.89";
-    // String PORT = ":8743/";
     /// String API_SERVER_URL = HOST + PORT+"iocm/app/";
     //  String API_SERVER_URL = "https://www.12306.cn/mormhweb/";
 }
