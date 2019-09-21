02ad073ad2c4d3cd2bbfb3d5cecf33ce3bfd7cf2
张攀 <zhangp@oitsme.com>
2019-09-22 04:04:58 +0800

    remove download code

--- app/src/main/java/com/cypoem/retrofit/activity/MainActivity.java
+++ app/src/main/java/com/cypoem/retrofit/activity/MainActivity.java
@@ -1,52 +1,20 @@
 package com.cypoem.retrofit.activity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Looper;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.cypoem.retrofit.R;
 import com.cypoem.retrofit.module.reponse.LoginResponse;
 import com.cypoem.retrofit.module.request.ArticleWrapper;
 import com.cypoem.retrofit.net.RetrofitHelper;
-import com.cypoem.retrofit.net.ServerConfig;
-import com.zhpan.idea.net.common.BasicResponse;
 import com.zhpan.idea.net.common.DefaultObserver;
-import com.zhpan.idea.net.common.ProgressUtils;
-import com.zhpan.idea.net.download.DownloadListener;
-import com.cypoem.retrofit.net.DownloadHelper;
-import com.zhpan.idea.utils.FileUtils;
-import com.zhpan.idea.utils.LogUtils;
 import com.zhpan.idea.utils.RxUtil;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 
 
 public class MainActivity extends BaseActivity {
-    private Button btn;
-    ProgressBar progressBar;
-    TextView mTvPercent;
-    private DownloadHelper downloadHelper;
 
     @Override
     protected int getLayoutId() {
@@ -59,10 +27,6 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initView() {
-        progressBar = findViewById(R.id.progressBar);
-        mTvPercent = findViewById(R.id.tv_percent);
-        btn = findViewById(R.id.btn_download);
-        downloadHelper = new DownloadHelper();
     }
 
     /**
@@ -103,145 +67,4 @@ public class MainActivity extends BaseActivity {
                 });
     }
 
-    /**
-     * 单文件上传 方法一
-     */
-    public void uploadFile1(View view) {
-        //文件路径
-        File file = getFile();
-        RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-        MultipartBody.Builder builder = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("phone", "12345678901")
-                .addFormDataPart("password", "123123")
-                .addFormDataPart("uploadFile", file.getName(), fileBody);
-        List<MultipartBody.Part> parts = builder.build().parts();
-        RetrofitHelper.getApiService()
-                .uploadFiles(parts)
-                .subscribeOn(Schedulers.io())
-                .compose(this.<BasicResponse>bindToLifecycle())
-                .compose(ProgressUtils.<BasicResponse>applyProgressBar(this, "上传文件..."))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DefaultObserver<BasicResponse>() {
-                    @Override
-                    public void onSuccess(BasicResponse response) {
-                        showToast("文件上传成功");
-                    }
-                });
-    }
-
-    /**
-     * 单文件上传 方法二
-     */
-    public void uploadFile2(View view) {
-        File file = getFile();
-        //  图片参数
-        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-        MultipartBody.Part fileBody = MultipartBody.Part.createFormData("uploadFile", file.getName(), requestFile);
-        //  手机号参数
-        RequestBody phoneBody = RequestBody.create(MediaType.parse("multipart/form-data"), "12345678909");
-        //  密码参数
-        RequestBody pswBody = RequestBody.create(MediaType.parse("multipart/form-data"), "123123");
-
-        RetrofitHelper.getApiService()
-                .uploadFiles(phoneBody, pswBody, fileBody)
-                .subscribeOn(Schedulers.io())
-                .compose(this.<BasicResponse>bindToLifecycle())
-                .compose(ProgressUtils.<BasicResponse>applyProgressBar(this, "上传文件..."))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DefaultObserver<BasicResponse>() {
-                    @Override
-                    public void onSuccess(BasicResponse response) {
-                        showToast("文件上传成功");
-                    }
-                });
-    }
-
-    /**
-     * 下载文件
-     *
-     * @param view
-     */
-    public void download(View view) {
-        btn.setClickable(false);
-        downloadHelper.download(ServerConfig.DOWNLOAD_URL, new DownloadListener() {
-            @Override
-            public void onProgress(int progress) {
-                LogUtils.e("--------下载进度：" + progress);
-                Log.e("onProgress", "是否在主线程中运行:" + String.valueOf(Looper.getMainLooper() == Looper.myLooper()));
-                progressBar.setProgress(progress);
-                mTvPercent.setText(progress + "%");
-            }
-
-            @Override
-            public void onSuccess(ResponseBody responseBody) {  //  运行在子线程
-                saveFile(responseBody);
-                Log.e("onSuccess", "是否在主线程中运行:" + String.valueOf(Looper.getMainLooper() == Looper.myLooper()));
-            }
-
-            @Override
-            public void onFail(String message) {
-                btn.setClickable(true);
-                showToast("文件下载失败,失败原因：" + message);
-                Log.e("onFail", "是否在主线程中运行:" + String.valueOf(Looper.getMainLooper() == Looper.myLooper()));
-            }
-
-            @Override
-            public void onComplete() {  //  运行在主线程中
-                showToast("文件下载成功");
-                btn.setClickable(true);
-            }
-        });
-    }
-
-    /**
-     * 取消下载
-     *
-     * @param view
-     */
-    public void cancelDownload(View view) {
-        if (downloadHelper != null) {
-            downloadHelper.cancelDownload();
-            btn.setClickable(true);
-        }
-    }
-
-    private void saveFile(ResponseBody body) {
-        String fileName = "oitsme.apk";
-        String fileStoreDir = Environment.getExternalStorageDirectory().getAbsolutePath();
-        try {
-            InputStream is = body.byteStream();
-            File file = new File(fileStoreDir + "/" + fileName);
-            FileOutputStream fos = new FileOutputStream(file);
-            BufferedInputStream bis = new BufferedInputStream(is);
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = bis.read(buffer)) != -1) {
-                fos.write(buffer, 0, len);
-                fos.flush();
-            }
-            fos.close();
-            bis.close();
-            is.close();
-            installApk(file);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void installApk(File file) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(file),
-                "application/vnd.android.package-archive");
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
-    private File getFile() {
-        String fileStoreDir = Environment.getExternalStorageDirectory().getAbsolutePath();
-        String filePath = fileStoreDir + "/test/test.txt";
-        FileUtils.createOrExistsFile(filePath);
-        //文件路径
-        return new File(filePath);
-    }
 }
--- app/src/main/java/com/cypoem/retrofit/net/IdeaApiService.java
+++ app/src/main/java/com/cypoem/retrofit/net/IdeaApiService.java
@@ -3,27 +3,15 @@ package com.cypoem.retrofit.net;
 import com.cypoem.retrofit.module.reponse.LoginResponse;
 import com.cypoem.retrofit.module.request.ArticleWrapper;
 import com.cypoem.retrofit.module.request.LoginRequest;
-import com.zhpan.idea.net.common.BasicResponse;
-import com.zhpan.idea.net.download.DwonloadRequest;
 
-import java.util.List;
 import java.util.Map;
 
 import io.reactivex.Observable;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
 import retrofit2.http.Body;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
-import retrofit2.http.PartMap;
-import retrofit2.http.Query;
-import retrofit2.http.Streaming;
-import retrofit2.http.Url;
 
 /**
  * Created by dell on 2017/4/1.
@@ -51,53 +39,4 @@ public interface IdeaApiService {
     @POST("user/login")
     Observable<LoginResponse> login(@FieldMap Map<String, Object> map);
 
-
-    /**
-     * 单文件上传 方法一
-     * @param partList
-     * @return
-     */
-    @Multipart
-    @POST("upload/uploadFile.do")
-    Observable<BasicResponse> uploadFiles(@Part List<MultipartBody.Part> partList);
-
-    /**
-     * 单文件上传 方法二
-     * @return
-     */
-    @Multipart
-    @POST("upload/uploadFile.do")
-    Observable<BasicResponse<BasicResponse>> uploadFiles(@Part("phone") RequestBody phone,@Part("password") RequestBody password,@Part MultipartBody.Part image);
-
-    /**
-     * 多文件上传 方法一
-     * @param description
-     * @param imgs1
-     * @param imgs2
-     * @return
-     */
-    @POST("upload/uploadFile.do")
-    Observable<BasicResponse> uploadFiles(@Part("filename") String description,
-                                          @Part("pic\"; filename=\"image1.png") RequestBody imgs1,
-                                          @Part("pic\"; filename=\"image2.png") RequestBody imgs2);
-
-    /**
-     * 多文件上传 方法二
-     * @param description
-     * @param maps
-     * @return
-     */
-    @POST("upload/uploadFile.do")
-    Observable<BasicResponse> uploadFiles(@Part("filename") String description, @PartMap() Map<String, RequestBody> maps);
-
-    @Streaming
-    @GET//("download.do")
-    Observable<ResponseBody> download(@Url String url);//直接使用网址下载
-
-    @POST("file/download")
-    Observable<ResponseBody> downloadFile(@Body DwonloadRequest downloadRequest);
-
-    @FormUrlEncoded
-    @POST("file/download")
-    Observable<ResponseBody> downloadFile(@FieldMap Map<String, Object> map);
 }
